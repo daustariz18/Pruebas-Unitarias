25021ed68d5030194e5e6eac4b34e9b7a9a12fe7
Diego Ustariz <diusme@gmail.com>
2021-10-13 00:14:18 -0500

    prueba Unitarias V1

---
+++ test_calculadora.py
@@ -0,0 +1,49 @@
+import calculadora
+
+
+def test_suma():
+
+    # Arrange
+    numero_de_prueba = 4
+
+    # Act
+    numero_obtenido = calculadora.suma(2, 2)
+
+    # Assert
+    assert numero_obtenido == numero_de_prueba
+ 
+
+def test_resta():
+
+    # Arrange
+    numero_de_prueba = 0
+
+    # Act
+    numero_obtenido = calculadora.resta(2, 2)
+
+    # Assert
+    assert numero_de_prueba == numero_obtenido
+
+
+def test_multiplicacion():
+
+    # Arrange
+    numero_de_prueba = 4
+
+    # Act
+    numero_obtenido = calculadora.multiplicacion(2, 2)
+
+    # Assert
+    assert numero_de_prueba == numero_obtenido
+
+
+def test_division():
+
+    # Arrange
+    numero_de_prueba = 1
+
+    # Act
+    numero_obtenido = calculadora.division(2, 2)
+
+    # Assert
+    assert numero_de_prueba == numero_obtenido
